f46aace04b3c87e963ee9f9ef3e85b0c94229b97
akazemi24 <97485016+akazemi24@users.noreply.github.com>
2026-06-13 10:22:54 -0500

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(filter(str.isalnum, s))
+        s = s.lower()
+        j = len(s) - 1
+        for i in range(int(len(s) /2)):
+            if s[i] == s[j]:
+                j -= 1
+            else:
+                return False
+        return True
